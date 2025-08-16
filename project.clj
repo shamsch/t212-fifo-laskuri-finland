5237fd6049b620ec1068b14512eb9527af5960c4
Shamsur  Raza Chowdhury <shamsur314@gmail.com>
2025-08-16 17:52:06 +0300

    update project clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject t212-fifo-laskuri-finland "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tax calculator using FIFO method for Finnish capital gains tax on Trading 212 investment data"
+  :url "https://github.com/shamsch/t212-fifo-laskuri-finland"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
